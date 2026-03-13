d4e659b8f5441e577a6ebe77e71a1ef1c501089b
DevSumanP <siddharthachudy@gmail.com>
2026-03-13 15:49:16 +0545

    # Changes to be committed: #	deleted:    apps/web/app/(marketing)/page.tsx #	modified:   apps/web/app/page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import HomePage from "./(marketing)/page";
+'use client'
+export const dynamic = "force-dynamic"
 
-export default function Home() {
-  return (
-    <main>
-      <HomePage />
-    </main>
-  );
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
 }
